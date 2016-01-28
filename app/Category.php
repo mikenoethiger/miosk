72cca7406d66d1e35014618105cd2f4417ae1e6b
Mike Nöthiger <noethiger.mike@gmail.com>
2016-01-28 14:17:51 +0100

    stuff

--- app/Category.php
+++ app/Category.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+    ];
+
+    /**
      * Get the products for the category.
      */
     public function products()
